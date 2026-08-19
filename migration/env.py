5fbc4df383e3ae8184600d48a917f988cc60450f
Jack_Bob <vialkov.1999@gmail.com>
2026-08-19 23:11:01 +0200

    fix: complete persistence and migration hardening

--- migration/env.py
+++ migration/env.py
@@ -28,6 +28,7 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+    
 
 config.set_main_option("sqlalchemy.url", settings.database_url)
 # add your model's MetaData object here
@@ -55,7 +56,7 @@ def run_migrations_offline() -> None:
 
     """
     context.configure(
-        url=url,
+        url = config.get_main_option("sqlalchemy.url"),
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
